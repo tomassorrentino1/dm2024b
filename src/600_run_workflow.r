d3e9753c1f54dc0b5b0e42adea04597a64248989
Tomas Sorrentino <78612811+tomassorrentino1@users.noreply.github.com>
2024-07-14 22:43:33 +0000

    Add files via upload
    Scripts Finales Tomas Sorrentino

---
+++ src/600_run_workflow.r
@@ -0,0 +1,26 @@
+require("rlang")
+
+envg <- env()
+
+envg$EXPENV <- list()
+envg$EXPENV$repo_dir <- "~/dm2024b/"
+
+#------------------------------------------------------------------------------
+
+correr_workflow <- function( wf_scriptname )
+{
+  # creo el script que corre el experimento
+  comando <- paste0( "nice -n 15 Rscript --vanilla ",
+                     envg$EXPENV$repo_dir,
+                     wf_scriptname,
+                     "   ",
+                     wf_scriptname
+  )
+  
+  system( comando )
+}
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+
+# aqui efectivamente llamo al workflow
+correr_workflow( "src/workflows/619_workflow_semillereo1.r" )
